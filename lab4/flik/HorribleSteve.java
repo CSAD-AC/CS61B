00962ca252f809464cdc22f84149431ca349af3d
Zhu Chen <3029489598@qq.com>
2025-04-14 09:23:22 +0800

    lab4 except for git

--- lab4/flik/HorribleSteve.java
+++ lab4/flik/HorribleSteve.java
@@ -1,11 +1,25 @@
 package flik;
 
+import org.junit.Test;
+
 public class HorribleSteve {
     public static void main(String [] args) throws Exception {
         int i = 0;
         for (int j = 0; i < 500; ++i, ++j) {
             if (!Flik.isSameNumber(i, j)) {
                 throw new Exception(
+                        String.format("i:%d not same as j:%d ??", i, j));
+            }
+        }
+        System.out.println("i is " + i);
+    }
+
+    @Test
+    public void test() {
+        int i = 0;
+        for (int j = 0; i < 500; ++i, ++j) {
+            if (!Flik.isSameNumber(i, j)) {
+                throw new AssertionError(
                         String.format("i:%d not same as j:%d ??", i, j));
             }
         }
